0dc282f5bbac2db7b66b6f91ca5879850e197a16
beranemo <beranemo@gmail.com>
2018-04-08 06:41:44 +0800

    pass the 5% (2)

--- lib/cart.rb
+++ lib/cart.rb
@@ -11,7 +11,12 @@ class Cart
     @ordered_items = @ordered_items.delete_if{ |x| x == 0 } # 刪除值是 0 的
     
     if @ordered_items.size == 2
-      total = total + 2 * PRICE * 0.95
+      while @ordered_items.size > 0   # 當還有書籍時
+        total = total + 2 * PRICE * 0.95
+        @ordered_items[0] -= 1       # 刪除已計算過的書籍       
+        @ordered_items[1] -= 1       # 刪除已計算過的書籍
+        @ordered_items = @ordered_items.delete_if{ |x| x == 0 }
+      end
     else
       for item in @ordered_items do
         total += item * PRICE
