1e72cd9885308aab5693131ba1c01e0980a567db
beranemo <beranemo@gmail.com>
2018-04-08 06:38:50 +0800

    pass the 5% (1)

--- lib/cart.rb
+++ lib/cart.rb
@@ -8,9 +8,16 @@ class Cart
   
   def calculate
     total = 0
-    for item in @ordered_items do
-      total += item * PRICE
+    @ordered_items = @ordered_items.delete_if{ |x| x == 0 } # 刪除值是 0 的
+    
+    if @ordered_items.size == 2
+      total = total + 2 * PRICE * 0.95
+    else
+      for item in @ordered_items do
+        total += item * PRICE
+      end
     end
+    
     total
   end
 end
