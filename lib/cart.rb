57909659253353ac2f143e6a768125c9b5538425
beranemo <beranemo@gmail.com>
2018-04-08 06:54:48 +0800

    refactor

--- lib/cart.rb
+++ lib/cart.rb
@@ -2,31 +2,21 @@ class Cart
   PRICE = 100
   
   def add(items)
-    # 把資料由 hash 轉成 array
-    @ordered_items = items.map{ |key, value| value }
+    # 把資料由 hash 轉成 array 後，刪除資料為 0 的
+    @ordered_items = items.map{ |key, value| value }.delete_if{ |x| x == 0 }
   end
   
   def calculate
     total = 0
-    @ordered_items = @ordered_items.delete_if{ |x| x == 0 } # 刪除值是 0 的
     
-    if @ordered_items.size == 2
-      while @ordered_items.size > 0   # 當還有書籍時
-        if @ordered_items.size == 2
-          total = total + 2 * PRICE * 0.95
-          @ordered_items[0] -= 1       # 刪除已計算過的書籍       
-          @ordered_items[1] -= 1       # 刪除已計算過的書籍
-          @ordered_items = @ordered_items.delete_if{ |x| x == 0 }
-        else
-          total += PRICE               # 把客戶賣得單集 1 本計入總價
-          @ordered_items[0] -= 1       # 刪除已計算過的書籍
-          @ordered_items = @ordered_items.delete_if{ |x| x == 0 }
-        end
+    while @ordered_items.size > 0   # 當還有書籍時
+      if @ordered_items.size == 2
+        total += 2 * PRICE * 0.95
+      else
+        total += PRICE               # 把客戶賣得單集 1 本計入總價
       end
-    else
-      for item in @ordered_items do
-        total += item * PRICE
-      end
+      @ordered_items = @ordered_items.map{ |x| x -= 1 }
+      @ordered_items = @ordered_items.delete_if{ |x| x == 0 }
     end
     
     total
